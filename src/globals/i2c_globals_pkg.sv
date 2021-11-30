b540b7d0fbe4e60c0d8426a69fb1d60a96fcd263
chetansagabala <chetansagabala@mirafra.com>
2021-12-01 03:22:21 +0530

    updating files

--- src/globals/i2c_globals_pkg.sv
+++ src/globals/i2c_globals_pkg.sv
@@ -22,15 +22,18 @@ package i2c_globals_pkg;
  // The parameter for MAXIMUM_BITS supported per transfer
  parameter int MAXIMUM_BITS = 1024;
  
- //The parameter NO_OF_ROWS specifies the no of rows of an array
+ // The parameter NO_OF_ROWS specifies the no of rows of an array
  parameter int NO_OF_ROWS  = MAXIMUM_BITS/DATA_LENGTH;
  
+ // acknowledge bit or no acknowledge
+ parameter bit ack;
+
  // Enum: slave_address_width_e
  //  
  // Specifies the width of slave address
  //
- //SLAVE_ADDRESS_WIDTH_7 - specifies 7 bit slave address 
- //SLAVE_ADDRESS_WIDTH_10 - specifies 10 bit slave address
+ // SLAVE_ADDRESS_WIDTH_7 - specifies 7 bit slave address 
+ // SLAVE_ADDRESS_WIDTH_10 - specifies 10 bit slave address
  //
  typedef enum bit {
    SLAVE_ADDRESS_WIDTH_7 = 1'b0,
@@ -61,31 +64,31 @@ package i2c_globals_pkg;
    READ = 1'b1
  } read_write_e;
  
-// // struct: i2c_transfer_char_s
-// //
-// // sda: array which holds the sda transactions
-// // no_of_sda_bits_transfer: specifies how many sda bits to trasnfer 
-// //
-// typedef struct {
-// 
-//   bit[SLAVE_ADDRESS_WIDTH-1:0] slave_addr;
-//
-//   bit[REGISTER_ADDRESS_WIDTH-1:0]register_address;
-// 
-//   bit[NO_OF_ROWS-1][DATA_LENGTH-1:0] data;
-// 
-//   int no_of_data_bits_transfer; 
-// 
-// } i2c_transfer_data_s;
-// 
-// 
-// //struct: i2c_transfer_cfg_s
-// //read_write : read from or write to slave 
-// 
-// typedef struct {
-//   bit msb_first;
-//   bit read_write;
-// } i2c_transfer_cfg_s;
+ // struct: i2c_transfer_char_s
+ //
+ // sda: array which holds the sda transactions
+ // no_of_sda_bits_transfer: specifies how many sda bits to trasnfer 
+ //
+ typedef struct {
+ 
+   bit[SLAVE_ADDRESS_WIDTH-1:0] slave_address;
+
+   bit[REGISTER_ADDRESS_WIDTH-1:0] register_address;
+ 
+   bit[NO_OF_ROWS-1][DATA_LENGTH-1:0] data;
+ 
+   int no_of_data_bits_transfer; 
+ 
+ } i2c_transfer_data_s;
+ 
+ 
+ //struct: i2c_transfer_cfg_s
+ //read_write : read from or write to slave 
+ 
+ typedef struct {
+   bit msb_first;
+   bit read_write;
+ } i2c_transfer_cfg_s;
 
 
 endpackage : i2c_globals_pkg 
--- src/hvl_top/master/i2c_master_tx.sv
+++ src/hvl_top/master/i2c_master_tx.sv
@@ -1,7 +1,6 @@
 `ifndef I2C_MASTER_TX_INCLUDED_
 `define I2C_MASTER_TX_INCLUDED_
 
-  typedef enum {ADDRESS_7BIT=7, ADDRESS_10BIT=10} slave_address_e;
 
 //--------------------------------------------------------------------------------------------
 // Class: i2c_master_tx
@@ -12,21 +11,79 @@ class i2c_master_tx extends uvm_sequence_item;
   `uvm_object_utils(i2c_master_tx)
 
   rand bit [9:0]slave_address;
+  rand bit [6:0]slave_addr;
   rand bit read_write;
+  rand bit [7:0]reg_address;
+  rand bit [7:0]data;
+`ifndef I2C_MASTER_TX_INCLUDED_
+`define I2C_MASTER_TX_INCLUDED_
+
+
+//--------------------------------------------------------------------------------------------
+// Class: i2c_master_tx
+// <Description_here>
+//--------------------------------------------------------------------------------------------
+
+class i2c_master_tx extends uvm_sequence_item;
+  `uvm_object_utils(i2c_master_tx)
+
+  rand bit [9:0]slave_address;
+  rand bit [6:0]slave_addr;
+  rand bit read_write;
+  rand bit [7:0]reg_address;
   bit ack;
-  rand bit [7:0]write_data;
-  rand slave_address_e sl_addr_mode;
- 
+  rand bit [7:0]data;
+  rand slave_address_width_e sl_addr_mode;
+   
   //-------------------------------------------------------
   // Externally defined Tasks and Functions
   //-------------------------------------------------------
   extern function new(string name = "i2c_master_tx");
   extern function void do_print(uvm_printer printer);
 
-  constraint w_data{ write_data == 8'b0000_0000; }
+  constraint w_data{ data == 8'b0000_0000; }
   constraint s_addr{
-                    if(sl_addr_mode == ADDRESS_7BIT)  { slave_address == 7'b100_0000;}
-                    if(sl_addr_mode == ADDRESS_10BIT) { slave_address == 10'b00_0000_0001;}
+                    if(sl_addr_mode == SLAVE_ADDRESS_WIDTH_7)  { slave_addr == 7'b100_0000;}
+                    if(sl_addr_mode == SLAVE_ADDRESS_WIDTH_10) { slave_address == 10'b00_0000_0001;}
+  }
+  constraint slave_address_width_e {sl_addr_mode == ADDRESS_7BIT;}
+  //constraint read_write{read_write == 1'b0;}
+endclass : i2c_master_tx
+
+//--------------------------------------------------------------------------------------------
+// Construct: new
+//
+// Parameters:
+//  name - i2c_master_tx
+//  parent - parent under which this component is created
+//--------------------------------------------------------------------------------------------
+function i2c_master_tx::new(string name = "i2c_master_tx");
+  super.new(name);
+endfunction : new
+
+function void i2c_master_tx::do_print(uvm_printer printer);
+  super.do_print(printer);
+  printer.print_field("RD_WR", this.read_write, 1, UVM_BIN);
+  printer.print_field("SLAVE_ADDRESS", this.slave_address, 7, UVM_BIN);
+  printer.print_field("data", this.data, 8, UVM_BIN);
+endfunction    
+`endif
+
+  rand slave_address_width_e sl_addr_mode;
+   
+  
+  
+  
+  //-------------------------------------------------------
+  // Externally defined Tasks and Functions
+  //-------------------------------------------------------
+  extern function new(string name = "i2c_master_tx");
+  extern function void do_print(uvm_printer printer);
+
+  constraint w_data{ data == 8'b0000_0000; }
+  constraint s_addr{
+                    if(sl_addr_mode == SLAVE_ADDRESS_WIDTH_7)  { slave_addr == 7'b100_0000;}
+                    if(sl_addr_mode == SLAVE_ADDRESS_WIDTH_10) { slave_address == 10'b00_0000_0001;}
   }
   constraint slave_address_mode_c {sl_addr_mode == ADDRESS_7BIT;}
   //constraint read_write{read_write == 1'b0;}
@@ -47,7 +104,7 @@ function void i2c_master_tx::do_print(uvm_printer printer);
   super.do_print(printer);
   printer.print_field("RD_WR", this.read_write, 1, UVM_BIN);
   printer.print_field("SLAVE_ADDRESS", this.slave_address, 7, UVM_BIN);
-  printer.print_field("WRITE_DATA", this.write_data, 8, UVM_BIN);
+  printer.print_field("data", this.data, 8, UVM_BIN);
 endfunction    
 `endif
 
